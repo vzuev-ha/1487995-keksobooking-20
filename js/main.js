ee00acdf5e5633e0922df61d85dced735e8df660
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-06-08 00:41:43 +0300

    Добавлена функция генерации пустого объекта

--- js/main.js
+++ js/main.js
@@ -9,9 +9,8 @@ var X_MAX = 1100;
 var Y_MIN = 130;
 var Y_MAX = 630;
 
-// Для красоты все цены путь будут кратны 100. Соответственно, берем случайное количество "сотен".
-var PRICE_MIN_100 = 80;
-var PRICE_MAX_100 = 600;
+var PRICE_MIN = 8000;
+var PRICE_MAX = 60000;
 
 var ROOMS_MIN = 1;
 var ROOMS_MAX = 5;
@@ -43,9 +42,70 @@ function getRandomIntInclusive(min, max) {
 }
 
 /**
- * Генерирует случайный объект размещения
+ * Создает пустой Объект размещения
+ * @return {{offer: {features: [], rooms: number, address: string, checkin: string, price: number, guests: number, description: string, title: string, type: string, checkout: string, photos: []}, author: {avatar: string}, location: {x: number, y: number}}}
+ */
+function generateEmptyApartmentObject() {
+  return {
+    author: {
+      // строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём.
+      // Например, 01, 02 и т. д. Адреса изображений не повторяются
+      avatar: ''
+    },
+    offer: {
+      // строка, заголовок предложения
+      title: '',
+
+      // строка, адрес предложения. Для простоты пусть пока представляет собой запись вида
+      // "{{location.x}}, {{location.y}}", например, "600, 350"
+      address: '',
+
+      // число, стоимость
+      price: 0,
+
+      // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalow
+      type: '',
+
+      // число, количество комнат
+      rooms: 0,
+
+      // число, количество гостей, которое можно разместить
+      guests: 0,
+
+      // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+      checkin: '',
+
+      // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+      // checkout не может быть больше checkin, поэтому ограничиваем рандом!!!    <<<<<--- самовольное условие ;)
+      checkout: '',
+
+      // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+      features: [],
+
+      // строка с описанием,
+      description: '',
+
+      // массив строк случайной длины, содержащий адреса фотографий
+      // "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+      // "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
+      // "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+      photos: []
+    },
+    location: {
+      // случайное число, координата x метки на карте.
+      // Значение ограничено размерами блока, в котором перетаскивается метка.
+      x: 0,
+
+      // случайное число, координата y метки на карте от 130 до 630
+      y: 0
+    }
+  };
+}
+
+/**
+ * Генерирует случайный Объект размещения
  * @param {int} apartmentNumber Порядковый номер генерируемого объекта
- * @return {{offer: {features: [], rooms: int, address: string, checkin: (string), price: number, guests: int, description: string, title: string, type: (string), checkout: int, photos: []}, author: {avatar: string}, location: {x: int, y: int}}}
+ * @return {{offer: {features: *[], rooms: number, address: string, checkin: string, price: number, guests: number, description: string, title: string, type: string, checkout: string, photos: *[]}, author: {avatar: string}, location: {x: number, y: number}}}
  */
 function generateMockApartment(apartmentNumber) {
   var locationX = getRandomIntInclusive(X_MIN, X_MAX);
@@ -72,60 +132,27 @@ function generateMockApartment(apartmentNumber) {
     photosArray[i] = PHOTOS[i];
   }
 
-  return {
-    author: {
-      // строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём.
-      // Например, 01, 02 и т. д. Адреса изображений не повторяются
-      avatar: 'img/avatars/user0' + apartmentNumber + '.png'
-    },
-    offer: {
-      // строка, заголовок предложения
-      title: 'заголовок предложения',
+  // Создаем пустой Объект размещения и наполняем его данными
+  var apartment = generateEmptyApartmentObject();
 
-      // строка, адрес предложения. Для простоты пусть пока представляет собой запись вида
-      // "{{location.x}}, {{location.y}}", например, "600, 350"
-      address: locationX + ', ' + locationY,
+  apartment.author.avatar = 'img/avatars/user0' + apartmentNumber + '.png';
 
-      // число, стоимость
-      price: getRandomIntInclusive(PRICE_MIN_100, PRICE_MAX_100) * 100,
+  apartment.offer.title = 'заголовок предложения';
+  apartment.offer.address = locationX + ', ' + locationY;
+  apartment.offer.price = Math.floor(getRandomIntInclusive(PRICE_MIN, PRICE_MAX) / 100) * 100; // <<< ---- чтобы цены были красивые
+  apartment.offer.type = APARTMENT_TYPES[getRandomIntInclusive(0, APARTMENT_TYPES.length - 1)];
+  apartment.offer.rooms = getRandomIntInclusive(ROOMS_MIN, ROOMS_MAX);
+  apartment.offer.guests = getRandomIntInclusive(GUESTS_MIN, GUESTS_MAX);
+  apartment.offer.checkin = CHECK_INOUT_TIMES[checkInIndex];
+  apartment.offer.checkout = CHECK_INOUT_TIMES[getRandomIntInclusive(0, checkInIndex)];
+  apartment.offer.features = featuresArray;
+  apartment.offer.description = 'строка с описанием';
+  apartment.offer.photos = photosArray;
 
-      // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalow
-      type: APARTMENT_TYPES[getRandomIntInclusive(0, APARTMENT_TYPES.length - 1)],
+  apartment.location.x = locationX;
+  apartment.location.y = locationY;
 
-      // число, количество комнат
-      rooms: getRandomIntInclusive(ROOMS_MIN, ROOMS_MAX),
-
-      // число, количество гостей, которое можно разместить
-      guests: getRandomIntInclusive(GUESTS_MIN, GUESTS_MAX),
-
-      // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-      checkin: CHECK_INOUT_TIMES[checkInIndex],
-
-      // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-      // checkout не может быть больше checkin, поэтому ограничиваем рандом!!!    <<<<<--- самовольное условие ;)
-      checkout: getRandomIntInclusive(0, checkInIndex),
-
-      // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-      features: featuresArray,
-
-      // строка с описанием,
-      description: 'строка с описанием',
-
-      // массив строк случайной длины, содержащий адреса фотографий
-      // "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      // "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      // "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-      photos: photosArray
-    },
-    location: {
-      // случайное число, координата x метки на карте.
-      // Значение ограничено размерами блока, в котором перетаскивается метка.
-      x: locationX,
-
-      // случайное число, координата y метки на карте от 130 до 630
-      y: locationY
-    }
-  };
+  return apartment;
 }
 
 /**
