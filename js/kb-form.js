6c8be38b3bbe93efd2685ea9a1f7bdc7c92184b4
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-06-22 03:13:58 +0300

    14. Личный проект: модуляция (часть 1)

---
+++ js/kb-form.js
@@ -0,0 +1,85 @@
+'use strict';
+
+(function () {
+
+  /* *
+   * Обработчик изменения формы создания объявления. Единый для всех полей.
+   * @listens {event} evt Событие
+   */
+  function onAdFormChange(evt) {
+    // Делаем все проверки в одном обработчике, так легче управлять кодом
+    if (evt.target) {
+      switch (evt.target.id) {
+        case 'timein':
+          adForm.querySelector('#timeout').value = evt.target.value;
+          break;
+        case 'timeout':
+          adForm.querySelector('#timein').value = evt.target.value;
+          break;
+        case 'type':
+          adForm.querySelector('#price').min = window.kbConstants
+            .APARTMENT_TYPE_MIN_PRICES[window.kbConstants.APARTMENT_TYPES.indexOf(evt.target.value)];
+          break;
+        case 'room_number':
+          changeCapacityValidity(evt.target, adForm.querySelector('#capacity'));
+          break;
+        case 'capacity':
+          changeCapacityValidity(adForm.querySelector('#room_number'), evt.target);
+          break;
+      }
+    }
+  }
+
+  /**
+   * Установка CustomValidity для поля Capacity (вместимость)
+   * @param {HTMLSelectElement} roomInput Поле ввода количества комнат
+   * @param {HTMLSelectElement} guestInput Поле ввода количества гостей
+   */
+  function changeCapacityValidity(roomInput, guestInput) {
+    var roomNum = parseInt(roomInput.value, 10);
+    var guestNum = parseInt(guestInput.value, 10);
+
+    // Найдем массив возможных значений Кол-ва гостей для данного Кол-ва комнат
+    // Делаем это при помощи двух массивов - один с ключами, другой - со значениями
+    // Поскольку find по массиву объектов нам пока нельзя ;)
+    var roomValue = window.kbConstants.ROOMS_VALUE[window.kbConstants.ROOMS_KEY.indexOf(roomNum)];
+    if (roomValue) {
+      // Если такое количество гостей во-можно - стираем ошибку, иначе - обновляем из массива
+      if (roomValue.guests.includes(guestNum)) {
+        guestInput.setCustomValidity('');
+      } else {
+        guestInput.setCustomValidity(roomValue.message);
+      }
+    } else {
+      guestInput.setCustomValidity('');
+    }
+  }
+
+  /**
+   * Устанавливает или снимает всем элементам в коллекции свойство disabled
+   * @param {NodeListOf<Element>} controlsArray Коллекция элементов
+   * @param {boolean} isEnabled Доступен?
+   */
+  function switchControlsAccess(controlsArray, isEnabled) {
+    for (var i = 0; i < controlsArray.length - 1; i++) {
+      controlsArray[i].disabled = !isEnabled;
+    }
+  }
+
+
+  // Инициализация
+  // Найдем форму создания объявления и экспортируем ее
+  var adForm = document.querySelector('.ad-form');
+
+  var addressField = adForm.querySelector('#address');
+
+  window.kbForm = {
+    adForm: adForm,
+    addressField: addressField,
+
+    onAdFormChange: onAdFormChange,
+    changeCapacityValidity: changeCapacityValidity,
+    switchControlsAccess: switchControlsAccess
+  };
+
+})();
---
+++ js/kb-map.js
@@ -0,0 +1,65 @@
+'use strict';
+
+(function () {
+  /**
+   * Генерация тестовых меток на карте
+   */
+  function generatePins() {
+    // Создадим и заполним фрагмент
+    var fragment = document.createDocumentFragment();
+    var pinsArray = [];
+
+    for (var i = 0; i < window.kbConstants.PINS_COUNT; i++) {
+      pinsArray[i] = window.kbPin
+        .generatePinFromTemplate(window.kbMockData.generateApartment(i + 1));
+      fragment.appendChild(pinsArray[i]);
+    }
+
+    // Создаем DocumentFragment, наполненный метками, и добавляем его в разметку
+    pinsPlaceholder.appendChild(fragment);
+  }
+
+  /**
+   * Заполнение поля адреса координатами метки
+   */
+  function fillAddressFromPinMain() {
+    var x = parseInt(mapPinMain.style.left + Math.round(mapPinMain.style.width / 2), 10);
+    var y = parseInt(mapPinMain.style.top + mapPinMain.style.height, 10);
+
+    window.kbForm.addressField.value = x + ', ' + y;
+  }
+
+  /* *
+   * Обработчик клика по главной метке
+   * @param {event} evt Событие
+   * @listens event
+   *
+   */
+  function onMapPinMainClick(evt) {
+    if (typeof evt === 'object' && evt.button === 0) {
+      // Активировать страницу
+      window.main.activatePage();
+
+      // Заполнить поле адреса
+      fillAddressFromPinMain();
+    }
+  }
+
+  // Инициализация
+
+  // Найдем блок для размещения меток
+  var pinsPlaceholder = document.querySelector('.map__pins');
+
+  // Главная метка на карте
+  var mapPinMain = document.querySelector('.map__pin--main');
+
+
+  window.kbMap = {
+    generatePins: generatePins,
+
+    fillAddressFromPinMain: fillAddressFromPinMain,
+
+    onMapPinMainClick: onMapPinMainClick
+  };
+
+})();
